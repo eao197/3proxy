5330c7d73cab8616f62a9544fa5d85752b7b9ca0
Yauheni Akhotnikau <eao197@gmail.com>
2019-07-23 11:31:27 +0300

    Debug authmode removed.

--- src/authradius.c
+++ src/authradius.c
@@ -325,9 +325,6 @@ int radsend(struct clientparam * param, int auth, int stop){
 	int vendorlen=0;
 	char buf[64];
 
-//FIXME: just for debugging!
-return 0;
-
 	if(!radiussecret || !nradservers) {
 		return 4;
 	}
