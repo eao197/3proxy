f98fc4c1d8b0f7fc9b412e824bea9946f40f42a3
Yauheni Akhotnikau <eao197@gmail.com>
2019-07-30 11:18:26 +0300

    Version string updated.

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION
-#define VERSION "3proxy-0.9-devel [client-bandlim+radius robin]"
+#define VERSION "3proxy-0.9-devel [client-bandlim+radius(robin,bandlim)]"
 #endif
 #ifndef BUILDDATE
 #define BUILDDATE ""
